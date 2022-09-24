64ac2364223708263101744799a64c48c5c25565
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2022-09-24 17:43:17 +0530

    initial commit

---
+++ NodeServingHTMLfiles.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const fs = require("fs");
+const fileContent = fs.readFileSync("CSS.Navigation.html");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-type": "text/html" });
+  res.end(fileContent);
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening on port no 8000");
+});
