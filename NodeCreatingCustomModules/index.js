d9b9cfc48bad7bdf4d74b2ff5ea24b61f8e6d2f2
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2022-09-26 13:47:00 +0530

    initial commit

---
+++ NodeCreatingCustomModules/index.js
@@ -0,0 +1,7 @@
+// const average= require("./mod");
+
+const mod = require("./mod");
+console.log(mod.name);
+
+// console.log(average([3,4]));
+console.log("This is index.js");
---
+++ NodeCreatingCustomModules/mod.js
@@ -0,0 +1,13 @@
+console.log("This is module");
+
+function average(arr) {
+  let sum = 0;
+  arr.forEach((element) => {
+    sum += element;
+  });
+  return sum / arr.length;
+}
+
+// module.exports = average;
+
+module.exports.name = "Akshad";
