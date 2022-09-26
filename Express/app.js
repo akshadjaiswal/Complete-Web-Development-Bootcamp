16d945b75ef39d2afb35dacb173be7d77cd3831e
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2022-09-26 16:53:52 +0530

    first express app backend

--- Express/app.js
+++ Express/app.js
@@ -1,1 +1,30 @@
-console.log("Installing  and exploring Express")
+const express = require("express");
+
+const app = express();
+const port = 80;
+
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .send("Thsi is home page of  my first express app with Akshad Jaiswal  ");
+});
+
+app.get("/about", (req, res) => {
+  res.end("Thsi is about page of my first express app with Akshad jaiswal  ");
+});
+
+app.post("/about", (req, res) => {
+  res.end(
+    "Thsi is post request about page of my first express app with Akshad jaiswal  "
+  );
+});
+
+app.get("/this", (req, res) => {
+  res
+    .status(404)
+    .send("The Page is not found on my website  akshadjaiswal.netlify.com ");
+});
+
+app.listen(port, () => {
+  console.log(`The application started successfully started on port ${port}`);
+});
