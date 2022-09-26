36ee817f48e9e1c972415729ed22aa5cdc73c67a
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2022-09-26 19:37:32 +0530

    initial commit

--- Express/app.js
+++ Express/app.js
@@ -1,7 +1,22 @@
 const express = require("express");
-
+const path = require("path");
 const app = express();
 const port = 80;
+
+// for serving static files
+app.use("/static", express.static("static"));
+
+// set the template engine for pug
+app.set("view engine", "pug");
+
+//set the views directory
+app.set("views",path.join(__dirname, 'views'));
+
+//our pug demo endpoint
+app.get("/demo", (req,res)=>{
+  res.status(200).render('demo', { title: 'Hey Akshad', message: 'Hello there and we are here using pug in  the express for backend' })
+
+});
 
 app.get("/", (req, res) => {
   res
