8df3b8a238a20abf06ca3d1b58cea0360145954d
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2022-09-29 14:02:33 +0530

    Backend development for form

--- Express/app.js
+++ Express/app.js
@@ -1,12 +1,14 @@
 const express = require("express");
+const { fstat } = require("fs");
 const path = require("path");
 // const { title } = require("process");
-constfs = require("fs");
+const fs = require("fs");
 const app = express();
 const port = 80;
 
 //EXPRESS SPECIFIC STUFF
 app.use("/static", express.static("static")); // for serving static files
+app.use(express.urlencoded())
 
 //PUG SPECIFIC STUFF
 app.set("view engine", "pug"); // set the template engine for pug
@@ -16,6 +18,20 @@ app.set("views", path.join(__dirname, "views")); //set the views directory
 app.get('/',(req,res)=>{
   const con="This is the best content surf on thje internet so far so use it "
   const params={'title': 'Pubg is the best game',"content": con}
+  res.status(200).render('index.pug', params);
+})
+
+app.post('/',(req,res)=>{
+  name =req.body.name
+  age=req.body.age
+  gender=req.body.gender
+  address=req.body.address
+  more=req.body.more
+
+  let outputToWrite= `The name of the client : ${name}, Age of the client:${age},
+   Address of the client:${address}, More about client:${more}`
+   fs.writeFileSync('output.txt',outputToWrite)
+  const params={'message': 'Your form has been submitted successfully'}
     res.status(200).render('index.pug', params);
 })
 
