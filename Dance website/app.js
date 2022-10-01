ee993095bc22f0877fd8a1b7160f1d912b7d9b2e
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2022-10-01 12:19:30 +0530

    update

--- Dance website/app.js
+++ Dance website/app.js
@@ -14,11 +14,17 @@ app.set("views", path.join(__dirname, "views")); // Set the views directory
 // ENDPOINTS
 app.get("/", (req, res) => {
   const params = {};
-  res.status(200).render("index.pug", params);
+  res.status(200).render("home.pug", params);
+});
+
+app.get("/contact", (req, res) => {
+  const params = {};
+  res.status(200).render("contact.pug", params);
 });
 
 // START THE SERVER
 app.listen(port, () => {
   console.log(`The application started successfully on port ${port}`);
 });
+
 
