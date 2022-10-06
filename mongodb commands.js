0595997f2fcd1c717f2ffca5ca8dc93591ba6a9a
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2022-10-06 17:22:47 +0530

    update commands

--- mongodb commands.js
+++ mongodb commands.js
@@ -1,3 +1,5 @@
 // Inserting Data in MongoDb
 use akshadkart
-db.items.insertOne({name:"Samsung 30s"})
+db.items.insertOne({name:"Samsung 30s", price:42000, rating:4.5,qty:233,sold:98})
+
+db.items.insertMany([{name:"Samsung 30s", price:42000, rating:4.5,qty:233,sold:98},{name:"Realme 6", price:16000, rating:4.5,qty:355,sold:224}])
