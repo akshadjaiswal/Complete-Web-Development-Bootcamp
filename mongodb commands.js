033171bf5ddc1658df45e30a8317e28e688132a7
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2022-10-06 17:58:55 +0530

    update data

--- mongodb commands.js
+++ mongodb commands.js
@@ -3,6 +3,6 @@ use akshadkart
 
 db.items.insertOne({name:"Samsung 30s", price:42000, rating:4.5,qty:233,sold:98})
 
-db.items.insertMany([{name:"Samsung 30s", price:42000, rating:4.5,qty:233,sold:98},{name:"Realme 6", price:16000, rating:4.5,qty:355,sold:224}])
+db.items.insertMany([{name:"Samsung 30s", price:42000, rating:4.5,qty:233,sold:98},{name:"Realme 6", price:16000, rating:4.5,qty:355,sold:224},{name:"Redme 10 pro max", price:20000, rating:4.7,qty:200,sold:124, isBig:true}])
 
 db.items.find() //to see the data
