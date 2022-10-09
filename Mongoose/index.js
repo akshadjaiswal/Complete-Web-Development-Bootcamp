155d00ad9f1245c3e0d554d403996abb223b1401
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2022-10-09 17:09:13 +0530

    update

--- Mongoose/index.js
+++ Mongoose/index.js
@@ -4,5 +4,5 @@ main().catch(err => console.log(err));
 
 async function main() {
   await mongoose.connect('mongodb://localhost:27017/akshadkart');
-  console.log("we are connected")
+  console.log("we are connected") //printing for checking that we are connected or not.
 }
