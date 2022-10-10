52fbe6a1b64f41b9ba50cd5d9825c732c3656df1
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2022-10-10 23:01:11 +0530

    update

--- Mongoose/index.js
+++ Mongoose/index.js
@@ -4,20 +4,37 @@ main().catch((err) => console.log(err));
 
 async function main() {
   await mongoose.connect("mongodb://localhost:27017/akshadkart");
-  console.log("we are connected"); //printing for checking that we are connected or not.
+  //console.log("we are connected"); //printing for checking that we are connected or not.
 }
+
 const kittySchema = new mongoose.Schema({
-  name: String
+  name: String,
 });
 
 kittySchema.methods.speak = function speak() {
-  const greeting =  "My name name is " + this.name;
+  const greeting = "My name name is " + this.name;
   console.log(greeting);
 };
 
+const Kitten = mongoose.model("akshadKitty", kittySchema);
 
-const Kitten = mongoose.model('Kitten', kittySchema);
+const akshadKitty = new Kitten({ name: "akshadKitty" });
+const akshadKitty2 = new Kitten({ name: "akshadKitty2" });
+// console.log(akshadKitty.name);
+// akshadKitty.speak();
 
-const akshadKitty = new Kitten({ name: 'akshadKitty' });
-console.log(akshadKitty.name);
-akshadKitty.speak(); 
+akshadKitty.save(function (err, akshadKitty) {
+  if (err) return console.log(err);
+
+  akshadKitty.speak();
+});
+
+akshadKitty2.save(function (err, k) {
+  if (err) return console.log(err);
+  // k.speak();
+});
+
+Kitten.find({ name: "akshadKitty2" }, function (err, kittens) {
+  if (err) return console.log(err);
+  console.log(kittens);
+});
