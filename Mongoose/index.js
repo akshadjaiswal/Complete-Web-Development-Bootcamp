79a2978aec8a3179b8385e177fbc8ad46f0100e7
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2022-10-10 21:41:45 +0530

    update

--- Mongoose/index.js
+++ Mongoose/index.js
@@ -6,3 +6,11 @@ async function main() {
   await mongoose.connect("mongodb://localhost:27017/akshadkart");
   console.log("we are connected"); //printing for checking that we are connected or not.
 }
+const kittySchema = new mongoose.Schema({
+  name: String
+});
+
+const Kitten = mongoose.model('Kitten', kittySchema);
+
+const akshadKitty = new Kitten({ name: 'akshadKitty' });
+console.log(akshadKitty.name); 
